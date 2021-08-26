35644fabc2af9c32c39158ad18f64b38a17a931f
Cherron Simes <cherron@developdbycherron.com>
2021-08-26 15:10:47 -0500

    Added timer logic

--- src/Components/PokeFetch/PokeFetch.js
+++ src/Components/PokeFetch/PokeFetch.js
@@ -1,45 +1,79 @@
-import React, { Component } from 'react'
+import React, { Component} from 'react'
 import './PokeFetch.css';
-
-
 class PokeFetch extends Component {
-  constructor() {
-    super()
-    this.state = {
-      pokeInfo: '',
-      pokeSprite: '',
-      pokeName: '',
+    constructor() {
+        super();
+        this.state = {
+            pokeInfo: "",
+            pokeSprite: "",
+            pokeName: "",
+            seconds: 10,
+        };
     }
-  }
 
-  fetchPokemon() {
-    let min = Math.ceil(1);
-    let max = Math.floor(152);
-    let pokeNum = Math.floor(Math.random() * (max - min) + min);
-    fetch(`https://pokeapi.co/api/v2/pokemon/${pokeNum}`, {
-      method: 'GET'
-    }).then(res => res.json())
-      .then(res => {
+  componentDidMount() {
+  this.myInterval = setInterval(() => {
+  const { seconds } = this.state
+  if(seconds > 0) {
+    this.setState(({ seconds }) => ({seconds: seconds - 1}))
+  } 
+  }, 1000)
+
+      let min = Math.ceil(1);
+      let max = Math.floor(152);
+      let pokeNum = Math.floor(Math.random() * (max - min) + min);
+      fetch(`https://pokeapi.co/api/v2/pokemon/${pokeNum}`, {
+        method: "GET",
+      })
+      .then((res) => res.json())
+      .then((res) => {
         this.setState({
           pokeInfo: res,
           pokeSprite: res.sprites.front_default,
           pokeName: res.species.name,
-        })
+        });
       })
-      .catch((err) => console.log(err))
-  }
+      .catch((err) => console.log(err));
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.seconds);
+    }
+    
 
   render() {
-    return (
-      <div className={'wrapper'}>
-        <button className={'start'} onClick={() => this.fetchPokemon()}>Start!</button>
-        <h1 className={'timer'} >Timer Display</h1>
-        <div className={'pokeWrap'}>
-          <img className={'pokeImg'} src={this.state.pokeSprite} />
-          <h1 className={'pokeName'}>{this.state.pokeName}</h1>
-        </div>
-      </div>
-    )
+    // imageStyle = {
+    //   filter: "brightness(0%)"
+    // }
+    // if (this.seconds > 0) {
+    //   imageStyle = {
+
+    //   }
+    // }
+      return (
+          <div className={"wrapper"}>
+              <button
+                  className={"start"}
+                  onClick={() => {this.componentDidMount()}}
+              >
+                  Start!
+              </button>
+              <h1 className={"timer"}>
+                  {this.state.seconds}
+              </h1>
+              <div className={"pokeWrap"}>
+                  <img
+                      alt=""
+                      className={"pokeImg"}
+                      // style={imageStyle}
+                        
+                      id={"hide"}
+                      src={this.state.pokeSprite}
+                  />
+                  <h1 className={"pokeName"}>{this.state.pokeName}</h1>
+              </div>
+          </div>
+      );
   }
 }
 
